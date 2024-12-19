ad5b54627d18ba124ef0145596605773b2a3a17c
datnt2996 <datnt2996@gmail.com>
2024-12-19 17:39:33 +0700

    fixed bugs

--- .github/scripts/utils.sh
+++
@@ -1,9 +0,0 @@
-get_message_information(path) {
-  AUTHOR=$GITHUB_TRIGGERING_ACTOR
-  AUTHOR=$(get_author_name "$AUTHOR")
-  MESSAGE_INFO=""
-  LINK_ACTION="https://github.com/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
-  MESSAGE_INFO+="- Author: $AUTHOR.\n"
-  MESSAGE_INFO+="- Link action: $LINK_ACTION.\n"
-  echo $MESSAGE_INFO
-}
